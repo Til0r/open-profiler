5c822d73f35bea7702b23428c3de41afc9cdb5ce
til0r <sntangelo@gmail.com>
2023-12-21 00:24:28 +0100

    feat(general): First draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,5 +12,71 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'open-profiler';
 
+  badges = [
+    { name: 'angular', iconName: 'angular' },
+    { name: 'typescript', iconName: 'typescript' },
+    { name: 'javascript', iconName: 'javascript' },
+    { name: 'tailwindcss', iconName: 'tailwindcss' },
+    { name: 'bootstrap', iconName: 'bootstrap' },
+    { name: 'reactivex', iconName: 'reactivex' },
+    { name: 'redux', iconName: 'redux' },
+    { name: 'Electron', iconName: 'Electron' },
+    { name: 'npm', iconName: 'npm' },
+    { name: 'Ionic', iconName: 'Ionic' },
+    { name: 'socket.io', iconName: 'socket.io' },
+    { name: 'python', iconName: 'python' },
+    { name: 'flask', iconName: 'flask' },
+    { name: 'webpack', iconName: 'webpack' },
+    { name: 'web3.js', iconName: 'web3.js' },
+    { name: 'solidity', iconName: 'solidity' },
+    { name: 'Firebase', iconName: 'Firebase' },
+    { name: 'mysql', iconName: 'mysql' },
+    { name: 'docker', iconName: 'docker' },
+    { name: 'Arduino', iconName: 'Arduino' },
+    { name: 'RaspberryPi', iconName: 'RaspberryPi' },
+  ];
+
+  projects = [
+    {
+      name: 'Project',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      heroLink: './assets/project.jpg',
+    },
+    {
+      name: 'Project',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      heroLink: './assets/project.jpg',
+    },
+    {
+      name: 'Project',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      heroLink: './assets/project.jpg',
+    },
+    {
+      name: 'Project',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      heroLink: './assets/project.jpg',
+    },
+    {
+      name: 'Project',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      heroLink: './assets/project.jpg',
+    },
+    {
+      name: 'Project',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      heroLink: './assets/project.jpg',
+    },
+    {
+      name: 'Project',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      heroLink: './assets/project.jpg',
+    },
+    {
+      name: 'Project',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      heroLink: './assets/project.jpg',
+    },
+  ];
   constructor() {}
 }
