8c053c223ef4952c7af6c91f5fda4de83b4bd295
til0r <sntangelo@gmail.com>
2024-01-29 14:02:16 +0100

    fix(general): Added external redirect on projects and relocated about description to innerHTML

--- src/app/shared/models/project.model.ts
+++ src/app/shared/models/project.model.ts
@@ -3,6 +3,7 @@ import { IconModel } from './icon.model';
 export interface ProjectModel {
   name: string;
   heroLink?: string;
+  redirect?: string;
   description: string;
   socials?: IconModel[];
 }
